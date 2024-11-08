5598bb4accbac602731b1ebbd20ea5979ab5fa33
vyn <jhovynn3@gmail.com>
2024-11-08 16:49:07 +0800

    Delete bookKeepers_apurado_backend directory

--- bookKeepers_apurado_backend/src/main/java/com/g4appdev/bookKeepers/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.g4appdev.bookKeepers.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.g4appdev.bookKeepers.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- bookKeepers_apurado_backend/src/main/java/com/g4appdev/bookKeepers/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.g4appdev.bookKeepers.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.g4appdev.bookKeepers.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
